f4f4c8dace39529f431459a8f50f4ba211d66466
Robel Getnet <robelgetnetrg@gmail.com>
2021-02-09 20:35:00 +0300

    Initial Commit

---
+++ Assets/Editor/PathEditor.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PathCreator))]
+public class PathEditor : Editor {
+
+    PathCreator _creator;
+    Path _path;
+
+    void OnEnable()
+    {
+        _creator = (PathCreator)target;
+        if (_creator.path == null)
+        {
+            _creator.CreatePath();
+        }
+        _path = _creator.path;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if (GUILayout.Button("Create New Path"))
+        {
+            Undo.RecordObject(_creator, "Create New Path");
+            _creator.CreatePath();
+            _path = _creator.path;
+            
+            SceneView.RepaintAll();
+        }
+        
+        if (GUILayout.Button("Toggle Closed Path"))
+        {
+            Undo.RecordObject(_creator, "Toggle Closed Path");
+            _path.ToggleClosed();
+            
+            SceneView.RepaintAll();
+        }
+        
+        if (GUILayout.Button("Auto Map"))
+        {
+            Undo.RecordObject(_creator, "AutoMap");
+            _path.AutoMap();
+            SceneView.RepaintAll();
+        }
+    }
+
+    void OnSceneGUI()
+    {
+        Input();
+        
+        Draw();
+    }
+
+    void Input()
+    {
+        Event guiEvent = Event.current;
+        
+        if (guiEvent.type == EventType.MouseDown && guiEvent.button == 0 && guiEvent.shift)
+        {
+            float distanceCamToEndAnchor = (Camera.current.transform.position - _path[_path.TotalPoints - 1]).magnitude;
+            
+            Ray mouseRay = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+            Vector3 worldMousePosition = mouseRay.GetPoint(distanceCamToEndAnchor);
+            
+            float yDir = mouseRay.direction.y;
+            
+            if (Math.Abs(yDir) > .01f)
+            {
+                float distanceToXzPlane = Mathf.Abs(mouseRay.origin.y / yDir);
+                worldMousePosition = mouseRay.GetPoint(distanceToXzPlane);
+            }
+            
+            Undo.RecordObject(_creator, "Add segment");
+            _path.AddSegment(worldMousePosition);
+        }
+    }
+    
+    void Draw()
+    {
+        Handles.color = Color.black;
+
+        for (int i = 0; i < _path.TotalSegments; i++)
+        {
+            Vector3[] points = _path.GetPointsInSegment(i);
+            
+            Handles.DrawLine(points[0], points[1]);
+            Handles.DrawLine(points[2], points[3]);
+            
+            Handles.DrawBezier(points[0], points[3], 
+                points[1], points[2], Color.green, null, 1f);
+        }
+        
+        Handles.color = Color.red;
+
+        for (int i = 0; i < _path.TotalPoints; i++)
+        {
+            Vector3 handlePosition = Handles.FreeMoveHandle(_path[i], 
+                Quaternion.identity, .1f, Vector2.zero, Handles.SphereHandleCap);
+            
+            if (_path[i] != handlePosition)
+            {
+                Undo.RecordObject(_creator, "Move Handle");
+                _path.MovePoint(i, handlePosition);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Path.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[System.Serializable]
+public class Path
+{
+    [SerializeField, HideInInspector]
+    private List<Vector3> points;
+    
+    [SerializeField, HideInInspector]
+    private bool closed;
+
+    public int TotalSegments => points.Count / 3;
+    public int TotalPoints => points.Count;
+    
+    public Vector3 this[int i] => points[i];
+    
+    public Path(Vector3 centre)
+    {
+        points = new List<Vector3>
+        {
+            centre + Vector3.left,
+            centre + (Vector3.left + Vector3.forward) * .5f,
+            centre + (Vector3.right + Vector3.back) * .5f,
+            centre + Vector3.right,
+        };
+    }
+
+    public void AddSegment(Vector3 anchorPosition)
+    {
+        //add new control point for the shared anchor of end anchor of last segment
+        points.Add(points[points.Count - 1] + (points[points.Count - 1] - points[points.Count - 2]));
+        
+        //add new control point for the end anchor of new segment
+        points.Add((points[points.Count - 1] + anchorPosition) * .5f);
+        
+        //add new end anchor for new segment
+        points.Add(anchorPosition);
+    }
+
+    public Vector3[] GetPointsInSegment(int i)
+    {
+        return  new Vector3[]
+        {
+            points[i * 3],
+            points[i * 3 + 1],
+            points[i * 3 + 2],
+            points[LoopIndex(i * 3 + 3)],
+        };
+    }
+
+    public void MovePoint(int i, Vector3 position)
+    {
+        //change in position
+        Vector3 deltaMove = position - points[i];
+        points[i] = position;
+
+        //anchor point
+        if (i % 3 == 0)
+        {
+
+            if (i + 1 < TotalPoints || closed)
+            {
+                points[LoopIndex(i + 1)] += deltaMove;
+            }
+
+            if (i - 1 >= 0 || closed)
+            {
+                points[LoopIndex(i - 1)] += deltaMove;
+            }
+        }
+
+        //control point
+        else
+        {
+            bool nextPointIsAnchor = (i + 1) % 3 == 0;
+
+            int correspondingControlIndex = nextPointIsAnchor ? i + 2 : i - 2;
+            int anchorIndex = nextPointIsAnchor ? i + 1 : i - 1;
+
+            if (correspondingControlIndex < points.Count && correspondingControlIndex >= 0 || closed)
+            {
+                correspondingControlIndex = LoopIndex(correspondingControlIndex);
+                anchorIndex = LoopIndex(anchorIndex);
+                
+                float distanceToCorrespondingControlIndex = (points[anchorIndex] - points[correspondingControlIndex]).magnitude;
+                
+                Vector3 directionFromAnchorToPosition = (points[anchorIndex] - position).normalized;
+                
+                points[correspondingControlIndex] = points[anchorIndex] + directionFromAnchorToPosition 
+                                                    * distanceToCorrespondingControlIndex;
+            }
+        }
+    }
+
+    public void AutoMap()
+    {
+        Vector3[] anchors = new Vector3[TotalSegments];
+
+        for (int i = 0; i < TotalSegments; i++)
+        {
+            anchors[i] = points[i * 3];
+        }
+
+        Vector3 lowestAnchor = anchors.OrderBy(a => a.z).ToArray()[0];
+
+        anchors = anchors.OrderBy(a =>
+        {
+            Vector3 delta = a - lowestAnchor;
+
+            return Mathf.Atan2(delta.z, delta.x) * Mathf.Rad2Deg;
+        }).ToArray();
+
+        for (int i = 0; i < TotalSegments; i++)
+        {
+            points[i * 3] = anchors[i];
+        }
+    }
+
+    public void ToggleClosed()
+    {
+        closed = !closed;
+
+        if (closed)
+        {
+            //add new control point for the shared anchor of end anchor of last segment
+            points.Add(points[points.Count - 1] + (points[points.Count - 1] - points[points.Count - 2]));
+            
+            points.Add(points[0] + (points[0] - points[1]));
+        }
+
+        else
+        {
+            points.RemoveRange(TotalPoints - 2, 2);
+        }
+    }
+
+    private int LoopIndex(int i)
+    {
+        return (i + points.Count) % points.Count;
+    }
+}
